eea9df20ba227d758c85f70cf7723c656cefea41
Cunningham <joncunningham@FVFY37C6J1WK.tld>
2022-03-31 08:43:52 -0500

    last commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let input = "y";
 let player = 0;
 let bot = 0;
-do{
-    alert("Welcome to Twenty in this game we will go against each other trying to get the closest to 20")
-    var value = 0;
-    alert("I'll go first")
+var value = 0;
+var pvalue = 0;
+var option = 0;
+
+function name(){
+    var nam = prompt("Hello, what is your name")
+    return nam;
+}
+
+function computer(){
     do{
+        if (pvalue > 20){ 
+            break;
+        }
         do{
             var ran = Math.floor(Math.random() * 11)
         }while(ran == 0)
@@ -18,15 +27,14 @@ do{
             break;
         }else if(value >= 16){
             alert(`I have ${value}. I think I'll stop here.`)
-            alert("Your turn")
         }
 
     }while(value < 16)
-    
-    var pvalue = 0;
-    var option = 0;
+    return value;
+}
 
-
+function player1(){
+    alert("You go first")
     do{
         if(value > 20){
             break;
@@ -39,16 +47,19 @@ do{
                 alert(`You got ${pvalue} and lost.`)
                 bot++;
             }else if(pvalue == 20){
-                break;
+                break; 
             }else{
-            option = window.prompt(`You have ${pvalue}. I had ${value}. If you want to draw press 1 if you want to stop press 2.`)
+            option = window.prompt(`You have ${pvalue}. If you want to draw press 1 if you want to stop press 2.`)
             }
         }
     }while((option != "2") && (pvalue < 20) && (input != "n"))
+    return pvalue;
 
+}
 
+function winning(pvalue , value){
     if((pvalue <= 20) && (value <= 20) && (pvalue > value) && (pvalue != 20) || (pvalue == 20)){
-        alert(`You got ${pvalue} and Won.`)
+        alert(`You got ${pvalue} and Won. I had ${value}`)
         player++;
     }else if(value > 20){
         alert(`I got ${value} and lost.`)
@@ -60,14 +71,22 @@ do{
         alert(`We got ${pvalue} and tied.`)
     }
     
-    input = window.prompt(`The score is ${bot} for me and ${player} for you. Would you like to play again? If yes press y if no press n`)
+    input = window.prompt(`The score is ${bot} for me and ${player} for you ${na}. Would you like to play again? If yes press y if no press n`)
+    console.log(`The score was ${player} for you and ${bot} for me`)
+}
+
+let na = name();
+alert(`Hello ${na}, Welcome to Twenty in this game we will go against each other trying to get the closest to 20`)
+
+do{
+    value = 0;
+    pvalue = 0;
+    var x = player1();
+    var y = computer();
+
+
+    winning(x, y);
+
     
-    if (input == "y"){
-        value = 0;
-        pvalue = 0;
-    }
-
-    console.log(value)
-    console.log(pvalue)
-
+    
 }while(input != "n")
